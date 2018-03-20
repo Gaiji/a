c4a4559af48885a69c3c6e7c5854b569d94cdf4c
Gaiji <34226052+Gaiji@users.noreply.github.com>
2018-03-20 12:06:22 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,18 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    request({
-        url: 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',
-        method: 'POST',
-        form: { apikey: process.env.API_KEY, query: message },
-        json:  true
-    }, (err, response, body) => {
-        if (body.status == 0) {
-            message.reply(message, `${body.results[0].reply} (${Math.ceil(body.results[0].perplexity * 100) / 100})`);
-        } else {
-            message.reply(message, `エラーたよ:fearful: [${body.status} ${body.message}]`);
-        }
-    });
+    request(url: 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',method: 'POST',form: { apikey: process.env.API_KEY, query: message },json:  true, function(error, response, body) {
+            try {
+                message.reply(message, `${body.results[0].reply} (${Math.ceil(body.results[0].perplexity * 100) / 100})`);
+            } catch (err) {
+                message.channel.sendMessage("`Input was invalid`");
+            }
+        });
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
