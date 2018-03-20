4e75c94c5be08fbbdc0737b07dcd72fedaa2c3fe
Gaiji <34226052+Gaiji@users.noreply.github.com>
2018-03-20 11:55:14 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -7,6 +6,18 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    request({
+        url: 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',
+        method: 'POST',
+        form: { apikey: process.env.API_KEY, query: message },
+        json:  true
+    }, (err, response, body) => {
+        if (body.status == 0) {
+            message.reply(message, `${body.results[0].reply} (${Math.ceil(body.results[0].perplexity * 100) / 100})`);
+        } else {
+            message.reply(message, `エラーたよ:fearful: [${body.status} ${body.message}]`);
+        }
+    });
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
