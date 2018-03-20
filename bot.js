af287e2d817290c19621d51c7bb66390febf6673
Gaiji <34226052+Gaiji@users.noreply.github.com>
2018-03-20 12:33:51 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,13 +15,14 @@ client.on('message', (bot, message) => {
     request({
         url: 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',
         method: 'POST',
-        form: { apikey: process.env.API_KEY, query: message },
+        form: { apikey: process.env.API_KEY, query: bot },
         json:  true
     }, (err, response, body) => {
         if (body.status == 0) {
             bot.reply(`メッセージ: ${body.results[0].reply} (${Math.ceil(body.results[0].perplexity * 100) / 100})`);
         } else {
             bot.reply(`エラー: [${body.status} ${body.message}]`);
+            console.log(bot)
         }
     });
 });
